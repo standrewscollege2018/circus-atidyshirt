37f0d369ee49d1a0ab7965fcb62af57ab28a666d
Jordan <pyjordy@gmail.com>
2018-08-06 10:13:26 +1200

    Tkinters - Format
    Tkinter - First comit

---
+++ practise-assessment.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root = Tk()
+root.title("pizzas")
+root.geometry('450x300')
+
+class Ticket:
+    def __init__(self, time, capacity, cost):
+        self._time = time
+        self._capacity = capacity
+        self._cost = cost
+        tickets.append(self)
+        pizza_names.append(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
