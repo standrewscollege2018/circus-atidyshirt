f815843856a0356063d47a2670e42817fface141
Jordan <pyjordy@gmail.com>
2018-08-07 11:05:31 +1200

    Adding Lists, tickets, and starting functions.

--- practise-assessment.py
+++ practise-assessment.py
@@ -1,7 +1,7 @@
 from tkinter import *
 
 root = Tk()
-root.title("pizzas")
+root.title("Circus Tickets")
 root.geometry('450x300')
 
 class Ticket:
@@ -10,11 +10,19 @@ class Ticket:
         self._capacity = capacity
         self._cost = cost
         tickets.append(self)
-        pizza_names.append(name)
+        
 
+time = []
+capacity = []
+cost = []
+sold = 0
 
+Ticket("10am", 150, 5)
+Ticket("3pm", 150, 5)
+Ticket("8pm", 250, 12)
 
-
+selected_tickets = StringVar()
+selected_tickets.set(names[0])
 
 
 
